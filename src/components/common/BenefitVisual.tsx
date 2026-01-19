7081856e355aad8fc7b0a1a75c380b3f5f306b0b
HUGORDdev <juniordita78@gmail.com>
2026-01-19 17:44:43 +0100

    responsive

--- src/components/common/BenefitVisual.tsx
+++ src/components/common/BenefitVisual.tsx
@@ -4,12 +4,10 @@ import { FloatingItem } from "./HeroVisual"
 const BenefitVisual = () => {
     return (
         <div className="relative w-full max-w-lg mx-auto lg:mx-0 mt-10 lg:mt-0">
-            {/* L'image principale - Ajout d'une bordure subtile pour le contraste */}
             <div className="relative z-0 rounded-3xl overflow-hidden border border-gray-100 shadow-lg">
                 <img src="/pc.png" alt="Productivity" className="w-full h-auto object-cover" />
             </div>
 
-            {/* Total Income - Positionné différemment sur mobile */}
             <FloatingItem 
                 className="absolute bottom-[10%] -right-4 lg:bottom-[50%] lg:-right-22 bg-white p-3 md:p-4 rounded-xl shadow-xl flex items-center gap-3 z-10" 
                 delay={0.2}
@@ -23,7 +21,6 @@ const BenefitVisual = () => {
                 </div>
             </FloatingItem>
 
-            {/* Money Transfer - On le remonte un peu sur mobile pour éviter qu'il soit coupé */}
             <FloatingItem 
                 className="absolute -bottom-6 -left-4 lg:bottom-0 lg:-left-[20%] bg-white p-3 md:p-4 rounded-xl shadow-xl flex items-center gap-3 z-10" 
                 delay={0.2}
@@ -34,7 +31,6 @@ const BenefitVisual = () => {
                 </div>
             </FloatingItem>
 
-            {/* Amanda Young - Masqué sur mobile très petit ou simplifié */}
             <FloatingItem 
                 className="absolute -top-10 -left-4 lg:top-6 lg:-left-22 bg-white p-3 md:p-4 rounded-xl shadow-xl flex items-center gap-3 z-10 scale-90 md:scale-100" 
                 delay={0.2}
@@ -51,7 +47,6 @@ const BenefitVisual = () => {
                 </div>
             </FloatingItem>
 
-            {/* Icone Verte Flottante - Masquée sur mobile pour ne pas surcharger */}
             <FloatingItem 
                 className="hidden md:block absolute top-[30%] -left-16 lg:-left-28 bg-[#74C9A9] p-3 rounded-xl shadow-lg -rotate-15" 
                 delay={0.4}
